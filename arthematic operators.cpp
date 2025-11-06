a0e13dd865e394247461548d9ccb02ee06d17770
singerkamal143 <kamalkiran1204@gmail.com>
2025-11-06 15:09:07 +0530

    Add files via upload

---
+++ arthematic operators.cpp
@@ -0,0 +1,12 @@
+#include<stdio.h>
+int main()
+{
+	int a=3;
+	int b=5;
+	printf("addition of a and b is:%d\n",a+b);
+	printf("subraction of b from a is:%d\n",a-b);
+	printf("multiplication of a and b is:%d\n",a*b);
+	printf("quotient when a divided by b is:%d\n",a/b);
+	printf("remainder when a divided by b is:%d\n",a%b);
+	return 0;
+}
