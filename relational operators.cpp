3191dc57a98c3e8e587d9109c1da981c46480d43
singerkamal143 <kamalkiran1204@gmail.com>
2025-11-06 15:24:16 +0530

    Add files via upload

---
+++ relational operators.cpp
@@ -0,0 +1,13 @@
+#include<stdio.h>
+int main()
+{
+	int a=3;
+	int b=5;
+	printf("a is less than b is:%d\n",a<b);
+	printf("a is greater than b is:%d\n",a>b);
+	printf("a is less than or equal to b is:%d\n",a<=b);
+	printf("a is greater than or equal to b is:%d\n",a>=b);
+	printf("a is equal to b is:%d\n",a==b);
+	printf("a is not equal to b is:%d\n",a!=b);
+	return 0;
+}
