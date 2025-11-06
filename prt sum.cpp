c5fcb966d0dbbbe8868960e14878acffc4dc3095
singerkamal143 <kamalkiran1204@gmail.com>
2025-11-06 15:23:19 +0530

    Add files via upload

---
+++ prt sum.cpp
@@ -0,0 +1,13 @@
+#include<stdio.h>
+#include<math.h>
+int main()
+{
+	int p=5000;
+	int t=2;
+	int r=4;
+	float CI=p*(pow(1(+r/100),t)-1);
+	printf("compound interest is : %d",CI);
+	return 0;
+}
+	
+
