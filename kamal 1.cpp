094fbb60c184a11e53fa28b3b37d6d5dffeec51b
singerkamal143 <kamalkiran1204@gmail.com>
2025-11-06 15:18:30 +0530

    Add files via upload

---
+++ kamal 1.cpp
@@ -0,0 +1,13 @@
+//electricity bill
+#include<stdio.h>
+int main()
+{
+	  int units =10;
+	  int price = 100;
+	  float amount;
+	  amount = units*price;
+	  char name[10] = "kamal";
+	  printf("consumer name %s/n",name);
+      printf("amount of the bill %.3f" ,amount);
+	  return 0;
+}
