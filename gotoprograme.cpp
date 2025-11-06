b168226e8df9537328f681b7b8146d707547e4a8
singerkamal143 <kamalkiran1204@gmail.com>
2025-11-06 15:16:58 +0530

    Add files via upload

---
+++ gotoprograme.cpp
@@ -0,0 +1,38 @@
+#include<stdio.h>
+int main()
+{
+	int n = 4;
+	if (n == 4)  
+	
+        {
+        goto next;
+		printf("it is not three\n");
+	}
+next:
+	printf("it is three\n");
+	return 0;
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
