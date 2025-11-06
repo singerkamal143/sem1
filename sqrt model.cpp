4c3710e36eeb4bff2dd653bd931d4a56813e99df
singerkamal143 <kamalkiran1204@gmail.com>
2025-11-06 15:24:52 +0530

    Add files via upload

---
+++ sqrt model.cpp
@@ -0,0 +1,12 @@
+#include<stdio.h>
+#include<math.h>
+int main()
+{
+	int num;
+	float result;
+	printf("enter a number : ");
+	scanf("%d",&num);
+	result = sqrt(num);
+	printf("%f",result);
+	return 0;
+}
