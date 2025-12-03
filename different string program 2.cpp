0ccd3d61a53c8acefe305ed4f43a14db0ff44329
kamalkiran undefined <kamalkiran1204@gmail.com>
2025-12-03 13:22:26 +0530

    Add files via upload

---
+++ different string program 2.cpp
@@ -0,0 +1,16 @@
+#include<stdio.h>
+int main()
+{
+	
+	char s1[100], s2[100], result[200];
+	int choice, i, j, flag, len;
+	
+	printf("1. find length\n");
+    printf("2. copy string\n");
+    printf("3. compare length\n");
+    printf("4. concatenate length\n");
+    printf("5. convert to upper case\n");
+    printf("6. convert to lower case\n");
+    printf("7. find length\n");
+    printf(" ");
+    scanf("%d",&choice);
