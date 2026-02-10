ec17c94a66a756b3848650bc27d06657ed8dee9b
kamalkiran undefined <kamalkiran1204@gmail.com>
2026-02-10 14:03:51 +0530

    Add files via upload

---
+++ data structure ex 8.cpp
@@ -0,0 +1,24 @@
+#include<stdio.h>
+struct node
+{
+	int data;
+	struct node *next;
+	struct node *prev;
+};
+int main()
+{
+	struct node n1,n2,n3;
+	n1.data=90;
+	n2.data=78;
+	n3.data=43;
+	n1.next=&n2;
+	n2.next=&n3;
+	n3.next=NULL;
+	printf("%d->",n1.data);
+	printf("%d->",n1.next->data);
+	printf("%d->",n1.next->next->data);
+	printf("\n%d->",n3.data);
+	printf("%d->",n3.prev->data);
+	printf("%d->",n3.prev->prev->data);
+	
+}
